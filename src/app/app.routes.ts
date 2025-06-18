f31f3ecadf1956b3f7b2da9d49fc1ace5e7897c5
Ayoub Rezaei <ayoubrezaei79@gmail.com>
2025-06-18 10:05:30 +0200

    Define application routes for navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { DocumentationModule } from '@modules/documentation-module/documentation-module.component';
+import { ForDanielModule } from '@modules/for-daniel-module/for-daniel-module.component';
+import { PlaygroundModule } from '@modules/playground-module/playground-module.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'playground', component: PlaygroundModule },
+      { path: 'documentation', component: DocumentationModule },
+      { path: 'for-daniel', component: ForDanielModule },
+      { path: '', redirectTo: 'playground', pathMatch: 'full' },
+    ],
+  },
+];
