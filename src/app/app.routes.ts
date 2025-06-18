525e33dd1b22be8885070de922c9a1ec71860797
Ayoub Rezaei <ayoubrezaei79@gmail.com>
2025-06-18 15:39:55 +0200

    lazy load modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,35 @@
 import { Routes } from '@angular/router';
-import { DocumentationModule } from '@modules/documentation-module/documentation-module.component';
-import { ForDanielModule } from '@modules/for-daniel-module/for-daniel-module.component';
-import { PlaygroundModule } from '@modules/playground-module/playground-module.component';
 
 export const routes: Routes = [
   {
     path: '',
     children: [
-      { path: 'playground', component: PlaygroundModule },
-      { path: 'documentation', component: DocumentationModule },
-      { path: 'for-daniel', component: ForDanielModule },
-      { path: '', redirectTo: 'playground', pathMatch: 'full' },
+      {
+        path: 'playground',
+        loadComponent: () =>
+          import('@modules/playground-module/playground-module.component').then(
+            (m) => m.PlaygroundModule,
+          ),
+      },
+      {
+        path: 'documentation',
+        loadComponent: () =>
+          import('@modules/documentation-module/documentation-module.component').then(
+            (m) => m.DocumentationModule,
+          ),
+      },
+      {
+        path: 'for-daniel',
+        loadComponent: () =>
+          import('@modules/for-daniel-module/for-daniel-module.component').then(
+            (m) => m.ForDanielModule,
+          ),
+      },
+      {
+        path: '',
+        redirectTo: 'playground',
+        pathMatch: 'full',
+      },
     ],
   },
 ];
