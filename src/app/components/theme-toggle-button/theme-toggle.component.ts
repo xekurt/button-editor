a5201fd64563ee494cf17b498b1f007c6615ee27
Ayoub Rezaei <ayoubrezaei79@gmail.com>
2025-06-18 13:30:44 +0200

    generate theme toggle button

---
+++ src/app/components/theme-toggle-button/theme-toggle.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ThemeService } from '@services/theme.service';
+
+@Component({
+  selector: 'theme-toggle-button',
+  templateUrl: './theme-toggle.component.html',
+  imports: [CommonModule],
+})
+export class ThemeToggleButton {
+  themeService: ThemeService = inject(ThemeService);
+  toggleTheme() {
+    this.themeService.toggleTheme();
+  }
+  handleKeydown(event: KeyboardEvent) {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      this.toggleTheme();
+    }
+  }
+}
