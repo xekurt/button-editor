0e1d89c7d188f01d8225937b04a68185219405ee
Ayoub Rezaei <ayoubrezaei79@gmail.com>
2025-06-18 09:50:40 +0200

    Initialize angular project with tailwind

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DashboardLayout } from '@shared/layout/layout.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [DashboardLayout],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'WYSIWYG';
+}
