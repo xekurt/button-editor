d343f8e24cd08ee98f1b9e1e1c5c4a0950526ae5
Ayoub Rezaei <ayoubrezaei79@gmail.com>
2025-06-18 10:06:10 +0200

    Initialize for-daniel module

---
+++ src/app/modules/for-daniel-module/for-daniel-module.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-for-daniel-module',
+  imports: [],
+  templateUrl: './for-daniel-module.component.html',
+  styleUrl: './for-daniel-module.component.css',
+})
+export class ForDanielModule {}
