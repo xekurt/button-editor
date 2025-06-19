be3f9573129794fe4c81b9ff9eed66e2f8cec463
Ayoub Rezaei <ayoubrezaei79@gmail.com>
2025-06-19 22:21:20 +0200

    Refactor

--- src/app/modules/documentation-module/button-dropdown/button-dropdown.component.ts
+++ src/app/modules/documentation-module/button-dropdown/button-dropdown.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CustomButtonComponent } from '../../../components/custom-button/custom-button.component';
 
 @Component({
@@ -8,16 +8,15 @@ import { CustomButtonComponent } from '../../../components/custom-button/custom-
   templateUrl: './button-dropdown.component.html',
 })
 export class ButtonDropdownComponent {
-  @Input() index: number;
-  @Input() isOpen: boolean = false;
   @Input() variant: 'primary' | 'secondary' | 'tertiary';
-  @Output() toggleDropdown = new EventEmitter();
 
   statuses = ['Active', 'Inactive', 'Pending'];
   states: Array<'disabled' | 'loading'> = ['disabled', 'loading'];
   sizes: Array<'small' | 'medium' | 'large'> = ['small', 'medium', 'large'];
 
-  toggle(index) {
-    this.toggleDropdown.emit(index);
+  isOpen: boolean = false;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
   }
 }
--- src/app/modules/documentation-module/button-list/button-list.component.ts
+++ src/app/modules/documentation-module/button-list/button-list.component.ts
@@ -1,18 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { CustomButtonComponent } from '@components/custom-button/custom-button.component';
 import { ButtonDropdownComponent } from '../button-dropdown/button-dropdown.component';
 
 @Component({
   selector: 'app-button-list',
-  imports: [CommonModule, CustomButtonComponent, ButtonDropdownComponent],
+  imports: [CommonModule, ButtonDropdownComponent],
   templateUrl: './button-list.component.html',
 })
 export class ButtonListComponent {
   variants: Array<'primary' | 'secondary' | 'tertiary'> = ['primary', 'secondary', 'tertiary'];
-  openDropdowns: boolean[] = [true, false, false];
-
-  toggleDropdown(idx: number) {
-    this.openDropdowns[idx] = !this.openDropdowns[idx];
-  }
 }
