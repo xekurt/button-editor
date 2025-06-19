7f262bc16e746ca07e65b3980257b5f02036c8c3
Ayoub Rezaei <ayoubrezaei79@gmail.com>
2025-06-19 21:50:26 +0200

    Migrate button-list component to documentation module

--- src/app/modules/documentation-module/documentation-module.component.ts
+++ src/app/modules/documentation-module/documentation-module.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonListComponent } from './button-list/button-list.component';
 
 @Component({
   selector: 'app-documentation-module',
-  imports: [],
+  imports: [CommonModule, ButtonListComponent],
   templateUrl: './documentation-module.component.html',
   styleUrl: './documentation-module.component.css',
 })
