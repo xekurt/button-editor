676256e37b2d2519e51d9ccd878bcd4871aa66a5
Ayoub Rezaei <ayoubrezaei79@gmail.com>
2025-06-19 22:26:18 +0200

    clean up

--- src/app/modules/documentation-module/documentation-module.component.ts
+++ src/app/modules/documentation-module/documentation-module.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ButtonListComponent } from './button-list/button-list.component';
+import { ButtonListComponent } from './components/button-list/button-list.component';
 
 @Component({
   selector: 'app-documentation-module',
   imports: [CommonModule, ButtonListComponent],
   templateUrl: './documentation-module.component.html',
-  styleUrl: './documentation-module.component.css',
 })
 export class DocumentationModule {}
