41ffccbff3a316eebae14cd5107b49464ccf4488
Ayoub Rezaei <ayoubrezaei79@gmail.com>
2025-06-20 17:13:56 +0200

    Add ButtonEditorComponent to playground module imports

--- src/app/modules/playground-module/playground-module.ts
+++ src/app/modules/playground-module/playground-module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { PlaygroundDescriptionComponent } from '@modules/playground-module/components/playground-description/playground-description';
+import { ButtonEditorComponent } from './components/button-editor/button-editor';
 
 @Component({
   selector: 'playground-module',
-  imports: [CommonModule, PlaygroundDescriptionComponent],
+  imports: [CommonModule, PlaygroundDescriptionComponent, ButtonEditorComponent],
   templateUrl: './playground-module.html',
 })
 export class PlaygroundModule {}
